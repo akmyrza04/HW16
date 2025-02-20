b6ad022b5db2ef9ebd9002e266f5e125c3623ec9
rakhat04 <akmyrzaevrakhatr@gmail.com>
2025-02-20 11:57:28 +0500

    reg

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static final String VALID = "((([А-я]+)|([А-я-]+))\\s){2}(([А-я]+)|([А-я-]+))";
+    public static void main(String[] args) {
+        System.out.println("Аты-жөніңізді жазыңыз: ");
+        String info = new Scanner(System.in).nextLine().trim();
+
+        if(info.matches(VALID)){
+            String[] split = info.split("\\s+");
+            System.out.println("Тегіңіз: " + split[0]);
+            System.out.println("Есіміңіз: " + split[1]);
+            System.out.println("Әкеңіздің есімі: " + split[2]);
+        } else {
+            System.out.println("Қате тердіңіз");
+        }
     }
 }
